2bd3985edcf464eb48a5d6d5642de9779c94a506
yuchiu <yuchiu2002@hotmail.com>
2018-09-05 00:58:21 -0400

    use regular expression to filter out data, organize data in json format, use multi process to scrape multi pages

--- maoyan-top100/scraper.py
+++ maoyan-top100/scraper.py
@@ -1,6 +1,9 @@
 import requests  # pylint: disable=E0401
 from requests.exceptions import RequestException  # pylint: disable=E0401
 import os
+import json
+import re
+from multiprocessing import Pool
 import random
 USER_AGENTS_FILE = os.path.join(os.path.dirname(__file__), 'user_agents.txt')
 USER_AGENTS = []
@@ -21,6 +24,22 @@ def _get_headers():
     return headers
 
 
+def parse_one_page(html):
+    pattern = re.compile('<dd>.*?board-index.*?>(\d+)</i>.*?data-src="(.*?)".*?name"><a'
+                         + '.*?>(.*?)</a>.*?star">(.*?)</p>.*?releasetime">(.*?)</p>'
+                         + '.*?integer">(.*?)</i>.*?fraction">(.*?)</i>.*?</dd>', re.S)
+    items = re.findall(pattern, html)
+    for item in items:
+        yield{
+            'index': item[0],
+            'image': item[1],
+            'title': item[2],
+            'actor': item[3].strip()[3:],
+            'time': item[4].strip()[5:],
+            'rating': item[5]+item[6]
+        }
+
+
 def get_one_page(url):
     try:
         session_requests = requests.session()
@@ -32,11 +51,24 @@ def get_one_page(url):
         return None
 
 
-def main():
-    url = 'http://maoyan.com/board'
+def write_to_file(content):
+    with open('result.txt', 'a', encoding='utf-8') as f:
+        f.write(json.dumps(content, ensure_ascii=False) + '\n')
+        f.close()
+
+
+def main(offset):
+    url = 'http://maoyan.com/board/4?offset='+str(offset)
     html = get_one_page(url)
-    print(html)
+    for item in parse_one_page(html):
+        print(item)
+        write_to_file(item)
 
 
 if __name__ == '__main__':
-    main()
+    # multi process
+    pool = Pool()
+    pool.map(main, [i*10 for i in range(10)])
+    # #single process
+    # for i in range(10):
+    # main(i*10)
